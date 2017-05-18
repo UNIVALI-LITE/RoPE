be392cd40a7371600465c8b538da86d02148a776
unknown <pem@edu.univali.br>
2017-05-18 19:34:57 -0300

    Motor de passos funcionando a passos :)

--- FirmwareRope/FirmwareRope.ino
+++ FirmwareRope/FirmwareRope.ino
@@ -1,14 +1,11 @@
-#include "libs/AccelStepper/AccelStepper.cpp"
-#include "libs/ArduinoTimerObject/TimerObject.cpp"
 #include "libs/Button/Button.cpp"
+#include <Stepper.h>
 
-TimerObject *Timer = new TimerObject(0);
+#define PASSOS_IR 765
+#define PASSOS_GIRAR 330
 
-#define TEMPO_IR 3000
-#define TEMPO_GIRAR 1285
-
-#define VEL_SENTIDO_ANTIHORARIO 1000
-#define VEL_SENTIDO_HORARIO -1000
+#define VEL_SENTIDO_ANTIHORARIO 1
+#define VEL_SENTIDO_HORARIO -1
 
 #define QUANTIDADE_MAXIMA_ACOES 45
 
@@ -42,8 +39,9 @@ Button btnFrente = Button (A5);
 #define motor_esquerda_F3  8
 #define motor_esquerda_F4  9
 
-AccelStepper motor_direita(8, motor_direita_F1, motor_direita_F3, motor_direita_F2, motor_direita_F4);
-AccelStepper motor_esquerda(8, motor_esquerda_F1, motor_esquerda_F3, motor_esquerda_F2, motor_esquerda_F4);
+const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
+Stepper motor_direita(stepsPerRevolution, motor_direita_F1, motor_direita_F3, motor_direita_F2, motor_direita_F4);
+Stepper motor_esquerda(stepsPerRevolution, motor_esquerda_F1, motor_esquerda_F3, motor_esquerda_F2, motor_esquerda_F4);
 
 //Estados possíveis
 #define ESTADO_AGUARDANDO 1
@@ -62,47 +60,18 @@ int acoesContProg = 0;
 int acoes[QUANTIDADE_MAXIMA_ACOES] = {0};
 
 // !--- Acoes de Execucao ----
-void resetarMotores()
+
+void acionarMotores(int motor_direita_sentido, int motor_esquerda_sentido)
 {
-  motor_direita.setCurrentPosition(0);
-  motor_esquerda.setCurrentPosition(0);
-}
-
-void acionarMotores(int motor_direitaVel, int motor_esquerdaVel)
-{
-  motor_direita.setSpeed(motor_direitaVel);
-  motor_esquerda.setSpeed(motor_esquerdaVel);
-
-  motor_direita.runSpeed();
-  motor_esquerda.runSpeed();
-}
-
-void esperar(int duracao)
-{
-  Timer->setInterval(duracao);
-  Timer->setOnTimer(&pararEsperar);
-
-  emEspera = true;
-
-  Timer->Start();
-
-  while (emEspera)
-  {
-    Timer->Update();
-  }
-}
-
-void pararEsperar()
-{
-  emEspera = false;
-  Timer->Stop();
+  motor_direita.step((stepsPerRevolution*motor_direita_sentido) / 100);
+  motor_esquerda.step((stepsPerRevolution*motor_esquerda_sentido) / 100);
 }
 
 void feedback(int nota, int duracao, int led)
 {
   digitalWrite(led, HIGH);
   tone(SAIDA_SOM, nota);
-  esperar(duracao);
+  delay(duracao);
   noTone(SAIDA_SOM);
   digitalWrite(led, LOW);
 }
@@ -116,9 +85,9 @@ void feedbackEsquerda(bool programando)
       break;
     case false:
       feedback(440, 30, LED_ESQUERDA);
-      esperar(50);
+      delay(50);
       feedback(660, 30, LED_ESQUERDA);
-      esperar(50);
+      delay(50);
       feedback(880, 90, LED_ESQUERDA);
       break;
     default:
@@ -136,9 +105,9 @@ void feedbackDireita(bool programando)
       break;
     case false:
       feedback(880, 30, LED_DIREITA);
-      esperar(50);
+      delay(50);
       feedback(660, 30, LED_DIREITA);
-      esperar(50);
+      delay(50);
       feedback(440, 90, LED_DIREITA);
       break;
     default:
@@ -156,9 +125,9 @@ void feedbackFrente(bool programando)
       break;
     case false:
       feedback(880, 45, LED_FRENTE);
-      esperar(75);
+      delay(75);
       feedback(1320, 45, LED_FRENTE);
-      esperar(75);
+      delay(75);
       feedback(704, 135, LED_FRENTE);
       break;
     default:                                                                                                                                                
@@ -176,9 +145,9 @@ void feedbackTras(bool programando)
       break;
     case false:
       feedback(880, 45, LED_TRAS);
-      esperar(75);
+      delay(75);
       feedback(729, 45, LED_TRAS);
-      esperar(75);
+      delay(75);
       feedback(1056, 135, LED_TRAS);
       break;
     default:
@@ -195,93 +164,77 @@ void feedbackAguardando()
 void feedbackFim()
 {
       feedback(600, 45, LED_FRENTE);
-      esperar(75);
+      delay(75);
       feedback(900, 45, LED_DIREITA);
-      esperar(75);
+      delay(75);
       feedback(1200, 135, LED_TRAS);
-      esperar(75);
+      delay(75);
       feedback(1400, 135, LED_ESQUERDA);
-      esperar(75);
+      delay(75);
       feedback(900, 45, LED_TRAS);
-      esperar(75);
+      delay(75);
       feedback(700, 45, LED_DIREITA);
-      esperar(75);
+      delay(75);
       feedback(600, 135, LED_FRENTE);
 }
 
 void feedbackParar()
 {
    feedback(500, 45, LED_FRENTE);
-   esperar(50);
+   delay(50);
    feedback(400, 45, LED_DIREITA);
-   esperar(50);
+   delay(50);
    feedback(600, 135, LED_TRAS);
-   esperar(50);
+   delay(50);
    feedback(1000, 135, LED_ESQUERDA);
 }
 
 void girarEsquerda()
 {
-  Timer->setInterval(TEMPO_GIRAR);
-  Timer->setOnTimer(&pararMotor);
-
-  Timer->Start();
-
-  while (motorLigado && ESTADO_EXECUTANDO)
+  int passos = 0;
+    
+  while (passos < PASSOS_GIRAR && ESTADO_ATUAL == ESTADO_EXECUTANDO)
   {
-    acionarMotores(VEL_SENTIDO_HORARIO,VEL_SENTIDO_HORARIO);
-    Timer->Update();
+    acionarMotores(VEL_SENTIDO_HORARIO, VEL_SENTIDO_HORARIO);
     btnIr.process();
+    passos++;
   }
-  Timer->Stop();
 }
 
 void girarDireita()
 {
-  Timer->setInterval(TEMPO_GIRAR);
-  Timer->setOnTimer(&pararMotor);
-
-  Timer->Start();
-
-  while (motorLigado && ESTADO_EXECUTANDO)
+  int passos = 0;
+    
+  while (passos < PASSOS_GIRAR && ESTADO_ATUAL == ESTADO_EXECUTANDO)
   {
     acionarMotores(VEL_SENTIDO_ANTIHORARIO, VEL_SENTIDO_ANTIHORARIO);
-    Timer->Update();
     btnIr.process();
+    passos++;
   }
-  Timer->Stop();
 }
 
 void irFrente()
 {
-  Timer->setInterval(TEMPO_IR);
-  Timer->setOnTimer(&pararMotor);
-
-  Timer->Start();
-
-  while (motorLigado && ESTADO_EXECUTANDO)
+  int passos = 0;
+    
+  while (passos < PASSOS_IR && ESTADO_ATUAL == ESTADO_EXECUTANDO)
   {
-    acionarMotores(VEL_SENTIDO_HORARIO,VEL_SENTIDO_ANTIHORARIO);
-    Timer->Update();
+    acionarMotores(VEL_SENTIDO_HORARIO, VEL_SENTIDO_ANTIHORARIO);
     btnIr.process();
+    passos++;
   }
-  Timer->Stop();
 }
 
 void irTras()
 {
-  Timer->setInterval(TEMPO_IR);
-  Timer->setOnTimer(&pararMotor);
-
-  Timer->Start();
-
-  while (motorLigado && ESTADO_EXECUTANDO)
+  int passos = 0;
+    
+  while (passos < PASSOS_IR && ESTADO_ATUAL == ESTADO_EXECUTANDO)
   {
-    acionarMotores(VEL_SENTIDO_ANTIHORARIO,VEL_SENTIDO_HORARIO);
-    Timer->Update();
+    acionarMotores(VEL_SENTIDO_ANTIHORARIO, VEL_SENTIDO_HORARIO);
     btnIr.process();
+    passos++;
   }
-  Timer->Stop();
 }
 
 void verificarFeedback(int acoesContExec)
@@ -332,18 +285,11 @@ void verificarInstrucao(int acoesContExec)
   }
 }
 
-void pararMotor()
-{
-  motorLigado = false;
-}
-
 void executar() {
   motorLigado = true;
 
   if (acoesContExec < acoesContProg)
   {
-    resetarMotores();
-
     verificarFeedback(acoesContExec);
 
     verificarInstrucao(acoesContExec);
@@ -352,7 +298,7 @@ void executar() {
 
   acoesContExec++;
   
-  if (acoesContExec >= acoesContProg)
+  if (acoesContExec >= acoesContProg && ESTADO_ATUAL == ESTADO_EXECUTANDO)
   {
     feedbackFim();
   }
@@ -381,7 +327,6 @@ void desligarMotor()
 void reiniciarProgramacao()
 {
   desligarMotor();
-  Timer->Stop();
   zerarArrayInstrucoes();
   motorLigado = true;
   acoesContProg = 0;
@@ -399,18 +344,15 @@ void zerarArrayInstrucoes()
 
 void definirMotor()
 {
-  motor_direita.setMaxSpeed(2000);
-  motor_direita.setSpeed(VEL_SENTIDO_ANTIHORARIO);
-
-  motor_esquerda.setMaxSpeed(2000);
-  motor_esquerda.setSpeed(VEL_SENTIDO_ANTIHORARIO);
+  int motorSpeed = 155;
+  motor_direita.setSpeed(motorSpeed);
+  motor_esquerda.setSpeed(motorSpeed);
 }
 
 void onPress(Button &b)
 {
   if(ESTADO_ATUAL == ESTADO_EXECUTANDO){
     if(b.pin == btnIr.pin){
-      pararMotor();
       ESTADO_ATUAL = ESTADO_AGUARDANDO;
       feedbackParar();
     }
@@ -453,7 +395,7 @@ void onPress(Button &b)
   }
   
   
-  esperar(200);
+  delay(200);
 }
 
 void definirCallBack()
